e3933a048dd8c2258201835a9e7481133b9bd7df
Mike Zrimsek <mikezrimsek@gmail.com>
2017-02-05 20:36:56 -0500

    files for artist detail

---
+++ lib/components/artist_details_component/artist_details_component.dart
@@ -0,0 +1,11 @@
+import 'dart:async';
+
+import 'package:angular2/core.dart';
+import 'package:angular2/router.dart';
+
+import '../../models/artist.dart';
+
+class ArtistDetailsComponent extends Component
+{
+
+}
