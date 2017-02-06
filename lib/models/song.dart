c149882456ab12d1e34bfd13599ff9bf82f98ee9
Mike Zrimsek <mikezrimsek@gmail.com>
2017-02-06 02:35:49 -0500

    not going to show images for songs

--- lib/models/song.dart
+++ lib/models/song.dart
@@ -5,19 +5,16 @@ class Song
   String id;
   String name;
   String url;
-  String imageUrl;
 
-  Song(this.id, this.name, this.url, this.imageUrl);
+  Song(this.id, this.name, this.url);
 
   factory Song.fromJson(Map<String, dynamic> song)
   {
     var externalUrl = JsonHelper.getExternalUrl(song);
-    var imageUrl = JsonHelper.getImageUrl(song);
 
     return new Song(song['id'],
       song['name'],
-      externalUrl,
-      imageUrl
+      externalUrl
     );
   }
 }
