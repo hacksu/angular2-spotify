e73cf436577a4f5c45128c277076cd4bbb8d9b5d
Mike Zrimsek <mikezrimsek@gmail.com>
2017-02-06 01:53:14 -0500

    comments are useful here

--- lib/helpers/web_request_helper.dart
+++ lib/helpers/web_request_helper.dart
@@ -4,8 +4,9 @@ import 'package:http/http.dart';
 
 class WebRequestHelper
 {
+  //search
   static dynamic extractArtists(Response resp) => JSON.decode(resp.body)['artists']['items'];
-  
+  //artist details
   static dynamic extractArtist(Response resp) => JSON.decode(resp.body);  
   static dynamic extractAlbums(Response resp) => JSON.decode(resp.body)['items'];
   static dynamic extractSongs(Response resp) => JSON.decode(resp.body)['tracks'];
